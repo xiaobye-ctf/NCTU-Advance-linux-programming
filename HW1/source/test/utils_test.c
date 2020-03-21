ecd0ec45a31a6b1c28a2aefeaa1752712859d527
xiaobye <skps96g313@gmail.com>
2020-03-21 15:46:08 -0400

    add utils_test.c

---
+++ HW1/source/test/utils_test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<arpa/inet.h>
+void hex_to_ipv4(char *hex,char* str_ip){
+	struct in_addr addr;
+	sscanf(hex,"%X",&addr.s_addr);
+	char *tmp = inet_ntoa(addr);
+	sscanf(tmp,"%s",str_ip);
+}
+void hex_to_port(char* hex,int* int_port){
+	sscanf(hex,"%X",int_port);	
+}
+
+int main(void){
+	char ip[20];
+	int port;
+	hex_to_ipv4("0100007F",ip);
+	hex_to_port("18EB",&port);
+	printf("%s:%hu\n",ip,port);
+	return 0;
+}
