607daea9f0bdb03b573fe04842aca01d34e710f0
xiaobye <skps96g313@gmail.com>
2020-06-01 10:07:30 -0400

    add

--- HW3/solve_all.sh
+++ HW3/solve_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo addsub1:
-python addsub1.py|grep -o 'ASM{.*}'
+python addsub1.py | grep -o 'ASM{.*}'
 echo ''
 
 echo addsub2:
@@ -11,4 +11,10 @@ echo bubble:
 cat bubble.asm | nc aup.zoolab.org 2502 | grep -o 'ASM{.*}'
 echo ''
 
+echo clear17:
+cat clear17.asm | nc aup.zoolab.org 2503 | grep -o 'ASM{.*}'
+echo ''
 
+echo dec2ascii:
+cat dec2ascii.asm | nc aup.zoolab.org 2504 | grep -o 'ASM{.*}'
+echo ''
